4561d36939426d018c4d572f8ae147fb5517add2
Antoni Boucher <bouanto@zoho.com>
2016-09-15 21:53:06 -0400

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+/*
+ * TODO: use a dark GTK+ theme for showing a black status bar.
+ * TODO: write a webkit2 plugin to support scrolling.
+ */
+
+extern crate gtk;
+extern crate mg;
+#[macro_use]
+extern crate mg_settings;
+extern crate url;
+extern crate webkit2;
+extern crate xdg;
+
+use mg::Application;
+use mg_settings::Config;
+use url::Url;
+use webkit2::WebView;
+use xdg::BaseDirectories;
+
+use AppCommand::*;
+
+commands!(AppCommand {
+    Back,
+    Forward,
+    Open(String),
+    Quit,
+    Reload,
+    Reloadbypasscache,
+    Stop,
+});
+
+const APP_NAME: &'static str = env!("CARGO_PKG_NAME");
+
+fn main() {
+    gtk::init().unwrap();
+
+    let config = Config {
+        mapping_modes: vec!["n".to_string()],
+    };
+
+    let xdg_dirs = BaseDirectories::with_prefix(APP_NAME).unwrap();
+    let config_path = xdg_dirs.place_config_file("config")
+        .expect("cannot create configuration directory");
+
+    let app = Application::new_with_config(config);
+    let url_label = app.add_statusbar_item();
+
+    if let Err(error) = app.parse_config(config_path) {
+        // TODO: show the error in the status bar.
+        println!("{}", error);
+    }
+    app.set_window_title(APP_NAME);
+
+    let webview = WebView::new();
+    webview.load_uri("https://duckduckgo.com/lite/");
+
+    {
+        let app = app.clone();
+        webview.connect_load_changed(move |webview, _load_event| {
+            if let Some(url) = webview.get_uri() {
+                url_label.set_text(&url);
+            }
+
+            set_title(&app, webview);
+        });
+    }
+    {
+        let app = app.clone();
+        webview.connect_resource_load_started(move |webview, _resource, _uri_request| {
+            set_title(&app, webview);
+        });
+    }
+    app.set_view(&webview);
+
+    app.connect_command(move |command| {
+        match command {
+            Back => webview.go_back(),
+            Forward => webview.go_forward(),
+            Open(url) => {
+                if let Ok(_) = Url::parse(&url) {
+                    webview.load_uri(&url);
+                }
+                else {
+                    webview.load_uri(&format!("http://{}", url));
+                }
+            },
+            Quit => gtk::main_quit(),
+            Reload => webview.reload(),
+            Reloadbypasscache => webview.reload_bypass_cache(),
+            Stop => webview.stop_loading(),
+        }
+    });
+
+    gtk::main();
+}
+
+/// Set the title of the window as the progress and the web page title.
+fn set_title(app: &Application<AppCommand>, webview: &WebView) {
+    let progress = (webview.get_estimated_load_progress() * 100.0) as i32;
+    if let Some(title) = webview.get_title() {
+        if progress == 100 {
+            app.set_window_title(&format!("{} - {}", title, APP_NAME));
+        }
+        else {
+            app.set_window_title(&format!("[{}%] {} - {}", progress, title, APP_NAME));
+        }
+    }
+}
