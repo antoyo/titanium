41dc4f89e7bdb7396ddc44b4538af4496979f883
Antoni Boucher <bouanto@zoho.com>
2016-11-12 19:39:52 -0500

    Started to fix hover hinting not working.

--- titanium-web-extension/src/dom.rs
+++ titanium-web-extension/src/dom.rs
@@ -27,7 +27,6 @@ use webkit2gtk_webextension::{
     DOMDOMWindowExtManual,
     DOMElement,
     DOMElementExt,
-    DOMEventExt,
     DOMEventTarget,
     DOMEventTargetExt,
     DOMHTMLButtonElement,
@@ -37,6 +36,8 @@ use webkit2gtk_webextension::{
     DOMHTMLInputElement,
     DOMHTMLSelectElement,
     DOMHTMLTextAreaElement,
+    DOMMouseEvent,
+    DOMMouseEventExt,
     DOMNodeExt,
     DOMNodeList,
     WebPage,
@@ -54,7 +55,7 @@ macro_rules! return_if_disabled {
     };
 }
 
-/// A DOMElement iterator for a node list.
+/// A `DOMElement` iterator for a node list.
 pub struct ElementIter {
     index: u64,
     node_list: Option<DOMNodeList>,
@@ -111,18 +112,41 @@ pub fn get_document(page: &WebPage) -> Option<DOMElement> {
 }
 
 /// Get the position of an element relative to the screen.
-pub fn get_offset(element: &DOMElement) -> Pos {
+fn get_offset(element: &DOMElement) -> Pos {
+    let document = element.get_owner_document();
     let mut top = 0;
     let mut left = 0;
     let mut element = Some(element.clone());
+    let mut in_body = false;
     while let Some(el) = element {
         left += el.get_offset_left() as i64 - el.get_scroll_left();
         top += el.get_offset_top() as i64 - el.get_scroll_top();
         element = el.get_offset_parent();
+        if el.get_tag_name() == Some("BODY".to_string()) {
+            in_body = true;
+        }
+    }
+    let mut margin_top = 0;
+    if in_body {
+        // The margin top property only takes effect if the element is relative to the body.
+        // This means that static/fixed elements won't be affected by this margin.
+        if let Some(document) = document {
+            let property = document.get_elements_by_tag_name("html")
+                .and_then(|htmls| htmls.item(0))
+                .and_then(|html| html.downcast::<DOMElement>().ok())
+                .and_then(|html| document.get_default_view().map(|window| (window, html)))
+                .and_then(|(window, html)| window.get_computed_style(&html, None))
+                .and_then(|style| style.get_property_value("margin-top"));
+            if let Some(property) = property {
+                if let Ok(value) = property[..property.len() - 2].parse() {
+                    margin_top = value;
+                }
+            }
+        }
     }
     Pos {
         x: left,
-        y: top,
+        y: top + margin_top,
     }
 }
 
@@ -207,7 +231,7 @@ pub fn is_enabled(element: &DOMElement) -> bool {
 }
 
 /// Check if an element is hidden.
-/// This is not exactly the opposite as is_visible since is_hidden returns false for element that
+/// This is not exactly the opposite as `is_visible` since `is_hidden` returns false for element that
 /// are visible, but outside the viewport.
 pub fn is_hidden(document: &DOMDocument, element: &DOMElement) -> bool {
     if let Some(window) = document.get_default_view() {
@@ -248,26 +272,27 @@ pub fn is_visible(document: &DOMDocument, element: &DOMElement) -> bool {
         let height = window.get_inner_height();
         let width = window.get_inner_width();
         let pos = get_offset(element);
+        // FIXME: use the absolute position (including between frames).
         if pos.x < 0 || pos.x > width || pos.y < 0 || pos.y > height {
             return false;
         }
         let mut element = Some(element.clone());
         while let Some(el) = element {
-            if el.get_tag_name() == Some("BODY".to_string()) {
-                return true;
-            }
             if let Some(style) = window.get_computed_style(&el, None) {
                 if style.get_property_value("display") == Some("none".to_string()) ||
                     style.get_property_value("visibility") == Some("hidden".to_string()) ||
-                        style.get_property_value("opacity") == Some("0".to_string())
-                        {
-                            return false;
-                        }
+                    style.get_property_value("opacity") == Some("0".to_string())
+                {
+                    return false;
+                }
             }
-            element = el.get_offset_parent();
+            element = el.get_parent_element();
         }
+        true
     }
-    false
+    else {
+        false
+    }
 }
 
 /// Trigger a mouse down event on the element.
@@ -275,14 +300,27 @@ pub fn mouse_down(element: &DOMElement) {
     mouse_event("mousedown", element);
 }
 
+/* TODO: delete.
+/// Trigger a mouse enter event on the element.
+pub fn mouse_enter(element: &DOMElement) {
+    mouse_event("mouseenter", element);
+}*/
+
 /// Trigger a mouse event on the element.
 pub fn mouse_event(event_name: &str, element: &DOMElement) {
     let event = element.get_owner_document()
         .and_then(|document| document.create_event("MouseEvents").ok());
     if let Some(event) = event {
-        event.init_event(event_name, true, true);
-        let element: DOMEventTarget = element.clone().upcast();
-        element.dispatch_event(&event).ok();
+        let window = element.get_owner_document()
+            .and_then(|document| document.get_default_view());
+        if let Some(window) = window {
+            if let Ok(event) = event.downcast::<DOMMouseEvent>() {
+                // TODO: use the previously hovered element for the last parameter.
+                event.init_mouse_event(event_name, true, true, &window, 0, 0, 0, 0, 0, false, false, false, false, 0, element);
+                let element: DOMEventTarget = element.clone().upcast();
+                element.dispatch_event(&event).ok();
+            }
+        }
     }
 }
 
